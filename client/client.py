6622024e993e4927b73949dcc06f72ed9994d4f4
Alizon <alizonsalvador@yahoo.com>
2026-09-16 13:31:19 -0600

    Add recieve loop client.py

--- client/client.py
+++ client/client.py
@@ -1,14 +1,50 @@
 import socket
 import threading
+from PySide6.QtCore import QObject, Signal
 
-class ChatClient:
+class Client(QObject):
+
+    # Signals
+    message_rec = Signal(str)
+    disconnected = Signal()
+    error = Signal(str)
+
+
     def __init__(self, ip: str, port: int):
+        super().__init__()
 
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((ip, port))
 
+        self.running = True
+
+        self.receive_thread = threading.Thread(
+            target=None
+        )
+
+
     def send_message(self, message: str):
-        self.client.send(message.encode())
+        self.client.sendall(message.encode())
+
+    def _recieve_loop(self):
+        while self.running:
+            try:
+                message = self.client.recv(1024)
+
+                if not message:
+                    break
+
+                self.message_rec.emit(
+                    message.decode()
+                )
+
+            except OSError as e:
+                if self.running:
+                    self.error.emit(str(e))
+                break
+
+        self.disconnect.emit()
+
 
     def receive(self):
         return self.client.recv(1024).decode()
